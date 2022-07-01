6801b7a6e3cf67e52fe5a28096d3a7893124731f
Tornike Jijelava <tornike_jijelava@epam.com>
2022-07-01 17:22:19 +0400

    Add comlex interface

---
+++ src/Ship.java
@@ -0,0 +1,27 @@
+public class Ship implements Vehicle{
+
+    @Override
+    public void startEngine() {
+        // TODO
+    }
+
+    @Override
+    public void stopEngine() {
+        // TODO
+    }
+
+    @Override
+    public void accelerate() {
+        // TODO
+    }
+
+    @Override
+    public void fly() {
+        // TODO
+    }
+
+    @Override
+    public void swim() {
+        // TODO
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,7 @@
+public interface Vehicle {
+    void startEngine();
+    void stopEngine();
+    void accelerate();
+    void fly();
+    void swim();
+}

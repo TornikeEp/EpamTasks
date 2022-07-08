f6c41da4739e5b543932a3ee5c830bb2e039eb78
Tornike Jijelava <tornike_jijelava@epam.com>
2022-07-08 14:38:31 +0400

    Refactor code

--- src/service/SentenceFinder.java
+++ src/service/SentenceFinder.java
@@ -1,19 +1,28 @@
 package service;
 
+import org.junit.platform.commons.util.StringUtils;
+
 public final class SentenceFinder {
 
     /*
         Find the sentence containing the largest number of words in some given text.
     */
-    public static int getLongestSequenceSize(String s){
-        if(s == null || s.isEmpty()) return 0;
-        int result = 0;
-        String[] sentenceList = s.split("[!?.]+");
+    public static int getLongestSequenceSize(String text){
+        if(StringUtils.isBlank(text)) return 0;
+        int longestSequenceSize = 0;
+        String[] sentenceList = text.split("[!?.]+");
         for(int i = 0; i < sentenceList.length; i++){
-            if(sentenceList[i].length() > 1) sentenceList[i] = sentenceList[i].trim();
-            String words[] = sentenceList[i].split("\\s+");
-            if(words.length != 0 && !words[0].isEmpty()) result = Math.max(result, words.length);
+            longestSequenceSize = Math.max(countWordOfSentence(sentenceList[i]), longestSequenceSize);
         }
-        return result;
+        return longestSequenceSize;
+    }
+
+    public static int countWordOfSentence(String sentence){
+        if(sentence.length() > 1) sentence = sentence.trim();
+        String words[] = sentence.split("\\s+");
+        if(words.length != 0 && !words[0].isEmpty()) {
+            return words.length;
+        }
+        return 0;
     }
 }

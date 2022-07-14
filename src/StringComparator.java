9f6577420589e825f64bea5c504fad37edbf2ba6
Tornike Jijelava <tornike_jijelava@epam.com>
2022-07-14 13:39:51 +0400

    Implemented adding in TreeSet with comparator

---
+++ src/StringComparator.java
@@ -0,0 +1,20 @@
+import org.junit.platform.commons.util.StringUtils;
+import java.util.Comparator;
+
+public class StringComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String string1, String string2) {
+        if(string1 == string2) {
+            return 0;
+        }
+        if (StringUtils.isBlank(string1) || string1.length() < 2) {
+            return -1;
+        }
+        if (StringUtils.isBlank(string2) || string2.length() < 2) {
+            return 1;
+        }
+        return Character.compare(string1.charAt(1), string2.charAt(1));
+    }
+}
+

97127081d23850096b3462c53345482db9663c33
Tornike Jijelava <tornike_jijelava@epam.com>
2022-07-21 15:12:04 +0400

    Add comment

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@
     Task: Create final static collection. Add and change elements.
  */
 public class Main {
+
+    /*
+        There is no problem to add remove or update elements in static
+        final collection. If we try to change reference we will have error.
+        Collection is final and not immutable.
+     */
     public static void main(String[] args) {
         Library.addBook(new Book("Don Quixote", 1605, 400));
         Library.addBook(new Book("Jane Eyre", 1847, 350));

375bfb52e339da4473b54094748003699095aa47
Tornike Jijelava <tornike_jijelava@epam.com>
2022-08-03 14:28:16 +0400

    Add my String class

---
+++ src/MyString.java
@@ -0,0 +1,13 @@
+public class MyString {
+
+    /*
+        Task - create class with name String and use both string classes.
+     */
+    public static void main(java.lang.String[] args) {
+        String myString = new String(10);
+        System.out.println(myString.getSize());
+        myString.sayHello();
+        java.lang.String string = "Tornike";
+        System.out.println(string);
+    }
+}
